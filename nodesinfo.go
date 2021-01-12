8571756f0ebcbb3e82d4b7b3456d6aeb7e6df3b7
Julie Iskander <iskander.j@wehi.edu.au>
2021-01-12 19:16:02 +1100

    update Memory for drained and idle

--- nodesinfo.go
+++ nodesinfo.go
@@ -133,14 +133,13 @@ func ParseNodesDataMetrics(input []byte) map[MetricKey]float64 {
 	for _, line := range lines {
 		if strings.Contains(line, ":") {
 
-			feature := strings.TrimSpace(strings.Split(line, ":")[4])
-			state := strings.TrimSpace(strings.Split(line, ":")[5])
+			feature := strings.TrimSpace(strings.Split(line, ":")[3])
+			state := strings.TrimSpace(strings.Split(line, ":")[4])
 			alloc, _ := strconv.ParseFloat(strings.TrimSpace(strings.Split(line, ":")[0]), 64)
 			free, _ := strconv.ParseFloat(strings.TrimSpace(strings.Split(line, ":")[1]), 64)
-			//total, _ := strconv.ParseFloat(strings.TrimSpace(strings.Split(line, ":")[2]), 64)
-			s := strings.TrimSpace(strings.Split(line, ":")[3])
+			s := strings.TrimSpace(strings.Split(line, ":")[2])
 			_, ok := data[MetricKey{state, feature}]
-			log.Println(data[MetricKey{state, feature}])
+
 			if !ok {
 				data[MetricKey{state, feature}] = 0
 			}
@@ -216,7 +215,7 @@ func (nic *NodesInfoCollector) Collect(ch chan<- prometheus.Metric) {
 
 	}
 	//sinfo -e -o%e,%f,alloc --state allocated
-	cmd := exec.Command("sinfo", "-h", "-e", "--state=allocated", "-OAllocMem:10:,FreeMem:10:,Memory:10:,StateLong:10:,Features:10,:alloc")
+	cmd := exec.Command("sinfo", "-h", "-e", "--state=allocated", "-OAllocMem:10:,FreeMem:10:,StateLong:10:,Features:10,:alloc")
 	data := ParseNodesDataMetrics(NodesDataInfoData(cmd))
 
 	for d := range data {
@@ -225,7 +224,7 @@ func (nic *NodesInfoCollector) Collect(ch chan<- prometheus.Metric) {
 				data[d], d.state, d.feature)
 		}
 	}
-	cmd = exec.Command("sinfo", "-h", "-e", "--state=idle", "-OAllocMem:10:,FreeMem:10:,Memory:10:,StateLong:10:,Features:10,:free")
+	cmd = exec.Command("sinfo", "-h", "-e", "--state=idle", "-OAllocMem:10:,FreeMem:10:,StateLong:10:,Features:10,:free")
 	data = ParseNodesDataMetrics(NodesDataInfoData(cmd))
 	for d := range data {
 		if data[d] >= 0 {
@@ -233,7 +232,7 @@ func (nic *NodesInfoCollector) Collect(ch chan<- prometheus.Metric) {
 				data[d], d.state, d.feature)
 		}
 	}
-	cmd = exec.Command("sinfo", "-h", "-e", "--state=drained", "-OAllocMem:10:,FreeMem:10:,Memory:10:,StateLong:10:,Features:10,:drained")
+	cmd = exec.Command("sinfo", "-h", "-e", "--state=drained", "-OAllocMem:10:,FreeMem:10:,StateLong:10:,Features:10,:drained")
 	data = ParseNodesDataMetrics(NodesDataInfoData(cmd))
 	for d := range data {
 		if data[d] >= 0 {
@@ -241,7 +240,7 @@ func (nic *NodesInfoCollector) Collect(ch chan<- prometheus.Metric) {
 				data[d], d.state, d.feature)
 		}
 	}
-	cmd = exec.Command("sinfo", "-h", "-e", "--state=maint", "-OAllocMem:10:,FreeMem:10:,Memory:10:,StateLong:10:,Features:10,:maint")
+	cmd = exec.Command("sinfo", "-h", "-e", "--state=maint", "-OAllocMem:10:,FreeMem:10:,StateLong:10:,Features:10,:maint")
 	data = ParseNodesDataMetrics(NodesDataInfoData(cmd))
 	for d := range data {
 		if data[d] >= 0 {
@@ -250,7 +249,7 @@ func (nic *NodesInfoCollector) Collect(ch chan<- prometheus.Metric) {
 		}
 	}
 
-	cmd = exec.Command("sinfo", "-h", "-e", "--state=completing", "-OAllocMem:10:,FreeMem:10:,Memory:10:,StateLong:10:,Features:10,:completing")
+	cmd = exec.Command("sinfo", "-h", "-e", "--state=completing", "-OAllocMem:10:,FreeMem:10:,StateLong:10:,Features:10,:completing")
 	data = ParseNodesDataMetrics(NodesDataInfoData(cmd))
 	for d := range data {
 		if data[d] >= 0 {
